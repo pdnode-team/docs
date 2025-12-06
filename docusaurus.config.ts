e0bf390bf710295955b6bdf41f17503f6e4392c2
Bret <Bretren2013@gmail.com>
2025-12-05 22:01:26 -0800

    fix config bug

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -102,7 +102,7 @@ const config: Config = {
           items: [
             {
               label: 'Docs',
-              to: '/docs',
+              to: '/docs/intro',
             },
           ],
         },
