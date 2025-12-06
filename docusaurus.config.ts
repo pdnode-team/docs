357c4ca229f972c4a242b73a6c9564bb9d16d0a0
Bret <Bretren2013@gmail.com>
2025-12-05 22:11:58 -0800

    update editurl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -44,7 +44,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/pdnode-team/docs',
+            'https://github.com/pdnode-team/docs/tree/main/docs',
         },
         blog: {
           showReadingTime: true,
@@ -54,7 +54,7 @@ const config: Config = {
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          // editUrl:'',
+          editUrl:'https://github.com/pdnode-team/docs/tree/main/blog',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
