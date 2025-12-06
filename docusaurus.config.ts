1f9ec1fc2d02d6b183a405ce303bfb9e43df1ac6
Bret <Bretren2013@gmail.com>
2025-12-06 12:35:01 -0800

    Fix editurl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -44,7 +44,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/pdnode-team/docs/tree/main/docs',
+            'https://github.com/pdnode-team/docs/tree/main/',
         },
         blog: {
           showReadingTime: true,
@@ -54,7 +54,7 @@ const config: Config = {
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:'https://github.com/pdnode-team/docs/tree/main/blog',
+          editUrl:'https://github.com/pdnode-team/docs/tree/main/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
